6a6ad0c4d2252dd419acb8aec42af6701c23bf9c
Parash <parashgoswami@outlook.com>
2021-09-15 18:19:15 +0530

    Added Work Order Aggregate

--- Domian/WorkOrderAggregate/WorkOrder.cs
+++ Domian/WorkOrderAggregate/WorkOrder.cs
@@ -1,25 +1,11 @@
 using System;
+using System.Collections.Generic;
 using Domian.Common;
 
 namespace Domian.WorkOrderAggregate
 {
     public class WorkOrder : AuditableEntity, IAggregateRoot
     {
-        private WorkOrder()
-        {
-        }
-
-        public WorkOrder( string orderNo, DateTime orderDate, string title, string aggrementNo, int projectId,  int contractorId)
-        {
-            
-            OrderNo = orderNo;
-            OrderDate = orderDate;
-            Title = title;
-            AggrementNo = aggrementNo;
-            ProjectId = projectId;           
-            ContractorId = contractorId;
-           
-        }
 
         public int Id { get; private set; }
         public string OrderNo { get; private set; }
@@ -29,8 +15,32 @@ namespace Domian.WorkOrderAggregate
         public int ProjectId { get; private set; }
         public Project Project { get; private set; }
         public int ContractorId { get; private set; }
-        public Contractor Contractor { get; private set; }
 
+        public Contractor Contractor { get; private set; }
+        private readonly List<WorkOrderItem> _items = new List<WorkOrderItem>();
+        public IReadOnlyList<WorkOrderItem> Items => _items.AsReadOnly();
+
+        private WorkOrder()
+        {
+        }
+
+        public WorkOrder(string orderNo, DateTime orderDate, string title, string aggrementNo, int projectId, int contractorId)
+        {
+
+            OrderNo = orderNo;
+            OrderDate = orderDate;
+            Title = title;
+            AggrementNo = aggrementNo;
+            ProjectId = projectId;
+            ContractorId = contractorId;
+
+        }
+
+        public void AddLineItem(string name, int no, int uomId, int poQuantity)
+        {
+           _items.Add(new WorkOrderItem(name,no,uomId,poQuantity));
+        }
+        
 
     }
 }
--- Domian/WorkOrderAggregate/WorkOrderItem.cs
+++ Domian/WorkOrderAggregate/WorkOrderItem.cs
@@ -4,11 +4,26 @@ namespace Domian.WorkOrderAggregate
 {
     public class WorkOrderItem :AuditableEntity
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public int No { get; set; }   
-        public int UomId { get; set; } 
-        public Uom Uom { get; set; }
-        public int PoQuantity { get; set; }
+        private WorkOrderItem()
+        {
+        }
+
+        public WorkOrderItem(string name, int no, int uomId, int poQuantity)
+        {
+            
+            Name = name;
+            No = no;
+            UomId = uomId;            
+            PoQuantity = poQuantity;
+        }
+
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public int No { get; private set; }   
+        public int UomId { get; private set; } 
+        public Uom Uom { get; private set; }
+        public int PoQuantity { get; private set; }
+
+        
     }
 }
